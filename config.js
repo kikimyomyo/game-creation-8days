9d6f7c91646c693afe498b76d77f2ee74ae49ee2
kikimyomyo <request1004@gmail.com>
2026-05-09 02:59:30 +0900

    deploy: creation-8days

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "creation-8days",
+  title: "천지창조 8일 — 빛부터 에덴까지",
+  sheet_tab: "creation-8days",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
